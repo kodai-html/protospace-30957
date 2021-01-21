2df9b8c2c27a59a73b2e16816118132c21a722b2
kodai-html <daietto.72@gmail.com>
2021-01-21 13:02:01 +0900

    4

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,7 +2,6 @@ class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :update, :destroy]
   before_action :set_prototype, except: [:index, :new, :create]
-  before_action :contributor_confirmation, only: :edit
   
   
   def index
@@ -29,7 +28,7 @@ class PrototypesController < ApplicationController
   end
 
   def edit
-    
+    redirect_to root_path unless current_user == @prototype.user
   end
 
   def update
@@ -61,10 +60,6 @@ class PrototypesController < ApplicationController
     unless user_signed_in?
       redirect_to action: :index
     end
-  end
-
-  def contributor_confirmation
-    redirect_to root_path unless current_user == @prototype.user
   end
 end
 
