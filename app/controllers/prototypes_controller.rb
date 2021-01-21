2ae591bf46585388aea8b9ff1c9b75c1e1e122a6
kodai-html <daietto.72@gmail.com>
2021-01-21 12:59:09 +0900

    3

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:update, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
   before_action :set_prototype, except: [:index, :new, :create]
   before_action :contributor_confirmation, only: :edit
   
